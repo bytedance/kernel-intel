c2e0befc0b11317e9fcde5c9c462bbd3606c31f0
Fuad Tabba <tabba@google.com>
2021-08-17 09:11:28 +0100

    KVM: arm64: Add feature register flag definitions
    commit 95b54c3e4c92b9185b15c83e8baab9ba312195f6 upstream
    
    Add feature register flag definitions to clarify which features
    might be supported.
    
    Consolidate the various ID_AA64PFR0_ELx flags for all ELs.
    
    No functional change intended.
    
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Acked-by: Will Deacon <will@kernel.org>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20210817081134.2918285-10-tabba@google.com
    
    Signed-off-by: Rui Qi <qirui.001@bytedance.com>

--- arch/arm64/include/asm/cpufeature.h
+++ arch/arm64/include/asm/cpufeature.h
@@ -511,7 +511,7 @@ static inline bool id_aa64pfr0_32bit_el0(u64 pfr0)
 {
 	u32 val = cpuid_feature_extract_unsigned_field(pfr0, ID_AA64PFR0_EL0_SHIFT);
 
-	return val == ID_AA64PFR0_EL0_32BIT_64BIT;
+	return val == ID_AA64PFR0_ELx_32BIT_64BIT;
 }
 
 static inline bool id_aa64pfr0_sve(u64 pfr0)
