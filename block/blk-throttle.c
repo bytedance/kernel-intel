4f2aeed5751bd394724e97b4f8d9bbd387601c63
Yu Kuai <yukuai3@huawei.com>
2022-09-21 17:53:09 +0800

    blk-throttle: improve bypassing bios checkings
    commit 81c7a63abc7c0be572b4f853e913ce93a34f6e1b upstream
    
    "tg->has_rules" is extended to "tg->has_rules_iops/bps", thus bios that
    don't need to be throttled can be checked accurately.
    
    With this patch, bio will be throttled if:
    
    1) Bio is read/write, and corresponding read/write iops limit exist.
    2) If corresponding doesn't exist, corresponding bps limit exist and
    bio is not throttled before.
    
    Signed-off-by: Yu Kuai <yukuai3@huawei.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Link: https://lore.kernel.org/r/20220921095309.1481289-3-yukuai1@huaweicloud.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Jinke Han <hanjinke.666@bytedance.com>

--- block/blk-throttle.c
+++ block/blk-throttle.c
@@ -160,7 +160,8 @@ struct throtl_grp {
 	unsigned int flags;
 
 	/* are there any throtl rules between this group and td? */
-	bool has_rules[2];
+	bool has_rules_bps[2];
+	bool has_rules_iops[2];
 
 	/* internally used bytes per second rate limits */
 	uint64_t bps[2][LIMIT_CNT];
@@ -732,11 +733,16 @@ static void tg_update_has_rules(struct throtl_grp *tg)
 	struct throtl_data *td = tg->td;
 	int rw;
 
-	for (rw = READ; rw <= WRITE; rw++)
-		tg->has_rules[rw] = (parent_tg && parent_tg->has_rules[rw]) ||
+	for (rw = READ; rw <= WRITE; rw++) {
+		tg->has_rules_iops[rw] =
+			(parent_tg && parent_tg->has_rules_iops[rw]) ||
 			(td->limit_valid[td->limit_index] &&
-			 (tg_bps_limit(tg, rw) != U64_MAX ||
-			  tg_iops_limit(tg, rw) != UINT_MAX));
+			  tg_iops_limit(tg, rw) != UINT_MAX);
+		tg->has_rules_bps[rw] =
+			(parent_tg && parent_tg->has_rules_bps[rw]) ||
+			(td->limit_valid[td->limit_index] &&
+			 (tg_bps_limit(tg, rw) != U64_MAX));
+	}
 }
 
 static void throtl_pd_online(struct blkg_policy_data *pd)
@@ -2479,6 +2485,21 @@ void blk_tg_limit_wake(struct list_head *head)
 	}
 }
 
+static inline bool blk_should_throtl(struct bio *bio)
+{
+	struct throtl_grp *tg = blkg_to_tg(bio->bi_blkg);
+	int rw = bio_data_dir(bio);
+
+	/* iops limit is always counted */
+	if (tg->has_rules_iops[rw])
+		return true;
+
+	if (tg->has_rules_bps[rw] && !bio_flagged(bio, BIO_BPS_THROTTLED))
+		return true;
+
+	return false;
+}
+
 bool blk_throtl_bio(struct request_queue *q, struct blkcg_gq *blkg,
 		    struct bio *bio)
 {
@@ -2497,7 +2518,7 @@ bool blk_throtl_bio(struct request_queue *q, struct blkcg_gq *blkg,
 		blkg_rwstat_add(&tg->stat_ios, bio->bi_opf, 1);
 	}
 
-	if (!tg->has_rules[rw])
+	if (!blk_should_throtl(bio))
 		return false;
 
 	if (bio->bi_opf & REQ_META)
