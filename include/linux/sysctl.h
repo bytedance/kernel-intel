a351789d181ff600b9a1cf70cb2860a8be6d691c
zhuyinfeng01 <zhuyinfeng01@bytedance.com>
2023-08-22 18:41:00 +0800

    bytedance: ttcp: add proc interface for time-wait
    add sysctl_tcp_tw_timeout option to set TIME-WAIT timeout
    
    Signed-off-by: zhuyinfeng01 <zhuyinfeng01@bytedance.com>

--- include/linux/sysctl.h
+++ include/linux/sysctl.h
@@ -62,6 +62,8 @@ extern int proc_dou8vec_minmax(struct ctl_table *table, int write, void *buffer,
 			       size_t *lenp, loff_t *ppos);
 extern int proc_dointvec_jiffies(struct ctl_table *, int,
 				 void __user *, size_t *, loff_t *);
+extern int proc_dointvec_tw_jiffies_minmax(struct ctl_table *table, int write,
+					void __user *buffer, size_t *lenp, loff_t *ppos);
 extern int proc_dointvec_userhz_jiffies(struct ctl_table *, int,
 					void __user *, size_t *, loff_t *);
 extern int proc_dointvec_ms_jiffies(struct ctl_table *, int,
--- include/net/netns/ipv4.h
+++ include/net/netns/ipv4.h
@@ -169,6 +169,7 @@ struct netns_ipv4 {
 	int sysctl_tcp_rmem[3];
 	int sysctl_tcp_comp_sack_nr;
 	int sysctl_tcp_handshake_ts;
+	int sysctl_tcp_tw_timeout;
 	unsigned long sysctl_tcp_comp_sack_delay_ns;
 	struct inet_timewait_death_row tcp_death_row;
 	int sysctl_max_syn_backlog;
